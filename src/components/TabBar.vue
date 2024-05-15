89a8c4308bff88096020f13737a9b8a1600f26d6
JinTianBuXiangShangBan <shadow6217@gmail.com>
2024-05-15 10:22:56 +0800

    chore: commit about

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,28 +1,27 @@
 <script setup lang="ts">
-const { t } = useI18n()
-const active = ref(0)
-const route = useRoute()
+const { t } = useI18n();
+const active = ref(0);
+const route = useRoute();
 
 const display = computed(() => {
-  if (route.meta.level && route.meta.level !== 2)
-    return true
-  return false
-})
+  if (route.meta.level && route.meta.level !== 2) return true;
+  return false;
+});
 </script>
 
 <template>
   <van-tabbar v-show="display" v-model="active" route>
     <van-tabbar-item replace to="/">
-      {{ t('layouts.home') }}
+      {{ t("layouts.home") }}
       <template #icon>
         <div class="i-carbon:home" />
       </template>
     </van-tabbar-item>
-    <van-tabbar-item replace to="/about">
+    <!-- <van-tabbar-item replace to="/about">
       {{ t('layouts.about') }}
       <template #icon>
         <div class="i-carbon:information" />
       </template>
-    </van-tabbar-item>
+    </van-tabbar-item> -->
   </van-tabbar>
 </template>
